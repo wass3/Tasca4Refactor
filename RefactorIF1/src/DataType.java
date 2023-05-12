d186412e72f6c8ea84293297b3990036efc0708c
wass3 <unlessbbx03@gmail.com>
2023-05-12 10:33:06 +0200

    refactor 'gd' --- Changed switch statement to forEach

--- RefactorIF1/src/DataType.java
+++ RefactorIF1/src/DataType.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.util.Arrays;
 
 public class DataType {
     public static void main(String[] args) {
@@ -6,18 +7,24 @@ public class DataType {
         System.out.println(DataType.gd(702));
         System.out.println(DataType.gd(0));
     }
-    public static String gd(int code){
 
-        switch (code) {
-            case 702, 1082, 1083, 1114, 1184, 1266, 12403 -> {
+    private static final Integer[] arrDate = {702, 1082, 1083, 1114, 1184, 1266, 12403};
+    private static final Integer[] arrNumeric = {20, 21, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3724, 2769, 12396};
+    public static String gd(int code){
+        for (Integer num:
+             arrDate) {
+            if (num == code) {
                 return "DATE";
             }
-            case 20, 21, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3724, 2769, 12396 -> {
+        }
+
+        for (Integer num:
+                arrNumeric) {
+            if (num == code) {
                 return "NUMERIC";
             }
-            default -> {
-                return "STRING";
-            }
         }
+
+        return "STRING";
     }
 }
