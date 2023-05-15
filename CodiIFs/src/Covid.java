412db15898de9eba9f63ec3010fbd15723932e28
wass3 <unlessbbx03@gmail.com>
2023-05-15 10:38:03 +0200

    refactor 'covid' --- Le he añadido espacios y le he quitado tabulaciones, poco más.

--- CodiIFs/src/Covid.java
+++ CodiIFs/src/Covid.java
@@ -3,11 +3,9 @@ import java.util.List;
 
 public class Covid {
 
-    public static String covid(float bodyTemperature, boolean difficultyBreathing,
-                               boolean diabetes, boolean cancer, boolean isPregnant,
-                               boolean isOver60yearsold, boolean hepatic,
-                               boolean kidnevDisease, boolean respiratoryDisease, String provincia
-    ){
+    public static String covid(float bodyTemperature, boolean difficultyBreathing, boolean diabetes, boolean cancer,
+                               boolean isPregnant, boolean isOver60yearsold, boolean hepatic, boolean kidnevDisease,
+                               boolean respiratoryDisease, String provincia){
         List<Boolean> arrDiseases = Arrays.asList(difficultyBreathing, diabetes, cancer, isPregnant, isOver60yearsold, hepatic, kidnevDisease, respiratoryDisease);
 
         if (bodyTemperature>= 38){
